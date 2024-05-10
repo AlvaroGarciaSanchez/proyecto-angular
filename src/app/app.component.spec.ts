3cec72ca83d5b43c430d38408292865108bf8cef
AlvaroGS <alvaro.garciasanchez@cieep.com>
2024-05-10 16:02:04 +0200

    cambio de vista

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'PokedexAngular' title`, () => {
+  it(`should have the 'pokedex' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PokedexAngular');
+    expect(app.title).toEqual('pokedex');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PokedexAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokedex');
   });
 });
